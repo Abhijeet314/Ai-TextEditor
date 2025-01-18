49263d00aa22a2bb4eb590ba8721ffe7ceadd7b5
Abhijeet <mateabhijeet8@gmail.com>
2025-01-18 20:01:03 +0530

    work

--- src/app/doc/[id]/layout.tsx
+++ src/app/doc/[id]/layout.tsx
@@ -2,25 +2,15 @@ import RoomProvider from '@/components/RoomProvider';
 import React from 'react';
 import { auth } from '@clerk/nextjs/server';
 
-interface LayoutProps {
-  children: React.ReactNode;
-  params: {
-    id: string;
-  }
-}
-
 // Add explicit type definition for the layout component
-const Layout = async ({ children, params }: LayoutProps) => {
-  const { id } = params;
-  
-  // Protect the route
-  await auth();  // Make sure to await the auth call
-
+function layout({children, params: {id}} : {children: React.ReactNode, params: {id: string}}) {
+  auth.protect()
   return (
+
     <RoomProvider roomId={id}>
       {children}
     </RoomProvider>
-  );
-};
+  )
+}
+export default layout;
 
-export default Layout;
