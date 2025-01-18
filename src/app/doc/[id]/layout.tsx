017447872598c8fcd5e221324b76b31c7c11fcf2
Abhijeet <mateabhijeet8@gmail.com>
2025-01-18 19:44:01 +0530

    final

--- src/app/doc/[id]/layout.tsx
+++ src/app/doc/[id]/layout.tsx
@@ -2,9 +2,10 @@ import RoomProvider from '@/components/RoomProvider';
 import React from 'react';
 import { auth } from '@clerk/nextjs/server';
 
-async function layout({ children, params }: { children: React.ReactNode; params: {id : string}}) {
-  // Await the params to extract the id
-  const {id} = await params
+async function layout({ children, params }: { children: React.ReactNode; params: { id: string } }) {
+  // Access the id directly from params
+  const { id } = params;
+
   // Protect the route
   auth.protect();
 
